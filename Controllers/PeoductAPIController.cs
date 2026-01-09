454214e4accc4a2a798cc19cf919bc968c012a6c
tofael <tofael56@yahoo.com>
2026-01-10 00:38:12 +0600

    Delete Action

--- Controllers/PeoductAPIController.cs
+++ Controllers/PeoductAPIController.cs
@@ -16,11 +16,11 @@ namespace FMS.API.Controllers
             return Ok(ProductStore.GetAllProducts);
         }
 
-        [HttpGet("{id:int}",Name ="GetProduct")]
+        [HttpGet("{id:int}", Name = "GetProduct")]
         [ProducesResponseType(StatusCodes.Status200OK)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult <ProductDto> GetProduct(int id)
+        public ActionResult<ProductDto> GetProduct(int id)
         {
             if (id == 0)
             {
@@ -37,30 +37,48 @@ namespace FMS.API.Controllers
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public ActionResult<ProductDto> CreateProduct( [FromBody] ProductDto product)
+        public ActionResult<ProductDto> CreateProduct([FromBody] ProductDto product)
         {
             //if(!ModelState.IsValid)
             //{
             //    return BadRequest(ModelState);
             //}
             //custome model stte validation
-            if (ProductStore.GetAllProducts.FirstOrDefault(x => x.Name.ToLower() == product.Name.ToLower())!=null)
+            if (ProductStore.GetAllProducts.FirstOrDefault(x => x.Name.ToLower() == product.Name.ToLower()) != null)
             {
                 ModelState.AddModelError("", "Product Already Exists!");
                 return BadRequest(ModelState);
             }
-            if (product== null)
+            if (product == null)
             {
                 return BadRequest(product);
             }
-            if (product.Id>0)
+            if (product.Id > 0)
             {
                 return StatusCode(StatusCodes.Status500InternalServerError);
             }
             product.Id = ProductStore.GetAllProducts.OrderByDescending(x => x.Id).FirstOrDefault().Id + 1;
             ProductStore.GetAllProducts.Add(product);
             //return the route of inserted data get
-            return CreatedAtRoute("GetProduct",new {id= product.Id}, product);
+            return CreatedAtRoute("GetProduct", new { id = product.Id }, product);
+        }
+        [HttpDelete("{id:int}",Name="DeleteProduct")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult DeleteProduct(int id)
+        {
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+            var product = ProductStore.GetAllProducts.FirstOrDefault(x => x.Id == id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+            ProductStore.GetAllProducts.Remove(product);
+            return NoContent();
         }
     }
 }
